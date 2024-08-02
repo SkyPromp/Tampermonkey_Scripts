88f8993fa483ba420ab763c053f4197ea0fd6838
SkyPromp <44906497+SkyPromp@users.noreply.github.com>
2024-08-03 00:55:25 +0200

    Also allow to show more digits for cpm

--- More Accurate Typing Speed-0.0.2.user.js
+++ More Accurate Typing Speed-0.0.2.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         More Accurate WPM
+// @name         More Accurate Typing Speed
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
-// @description  Show more precise wpm after a race on TypeRacer
+// @version      0.0.2
+// @description  Show more tying speed digits after a race on TypeRacer
 // @author       SkyPromp
 // @match        https://play.typeracer.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=typeracer.com
@@ -14,16 +14,13 @@ function Log(message){
 }
 
 new MutationObserver(getScoreboard).observe(document.body, {subtree: true, childList: true});
-let wpm = document.getElementsByClassName()[0];
-
 
 function getScoreboard(mutations_list) {
     for (let mutation of mutations_list){
         for (let node of mutation.addedNodes){
             if (node.className === "tblOwnStatsNumber"){
                 handleScoreboardNode(node);
-            }
-            else if(node.className === "gwt-TabPanel"){
+            } else if(node.className === "gwt-TabPanel"){
                 //handleStats(node.querySelectorAll(".StatsTable tbody tbody .gwt-Label"));
                 handleStats(node.querySelectorAll(".gwt-Label"));
             }
@@ -32,7 +29,8 @@ function getScoreboard(mutations_list) {
 }
 
 function handleScoreboardNode(node){
-    if (node.textContent.split(" ").at(-1) === "wpm") {
+    let field = node.textContent.split(" ").at(-1);
+    if (field === "wpm" || field === "cpm") {
         node.textContent = node.title;
     }
 }
